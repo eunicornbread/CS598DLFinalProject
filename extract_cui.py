4e7d6d3c169add51507ccc7b09d50ffef9c1ffb8
eunicornbread <xz33@illinois.edu>
2022-05-07 21:52:21 -0500

    limit number of rows since it's taking way too long

--- extract_cui.py
+++ extract_cui.py
@@ -4,6 +4,8 @@ import os
 import argparse
 from sklearn.utils import shuffle
 
+METAMAP_LITE_DIR = "/mnt/c/Users/eunic/git-repos/cs598dl/public_mm_lite/"
+
 # Run this file to extract CUIs from clinical notes
 
 def extract_cui(text): 
@@ -12,11 +14,18 @@ def extract_cui(text):
     return " ".join([concept[4] for concept in concepts])
 
 def parse_arguments(parser):
-	"""Read user arguments"""
-	parser.add_argument(
-		"--noteevents_file", type=str, default='./NOTEEVENTS.csv', help="File path to MIMIC-III dataset NOTEEVENTS.csv"
+    """Read user arguments"""
+    parser.add_argument(
+	    "--noteevents_file", type=str, default='./NOTEEVENTS.csv', help="File path to MIMIC-III dataset NOTEEVENTS.csv"
 	)
-	args = parser.parse_args()
+    parser.add_argument(
+    "--metamap_lite_dir", type=str, default=METAMAP_LITE_DIR, help="Directory path to metamap lite"
+    )
+    parser.add_argument(
+    "--max_rows", type=int, default=1000, help="Maximum number of rows to extract"
+    )
+    args = parser.parse_args()
+    return args
 
 
 if __name__ == '__main__':
@@ -24,13 +33,13 @@ if __name__ == '__main__':
 		formatter_class=argparse.ArgumentDefaultsHelpFormatter
 	)
     ARGS = parse_arguments(PARSER)
-
+    
     df = pd.read_csv(ARGS.noteevents_file)
     df = df[~pd.isnull(df['CHARTTIME'])]
+    df = df[:ARGS.max_rows]
 
-
-    os.chdir("/mnt/c/Users/eunic/git-repos/cs598dl/public_mm_lite/")
-    mm = MetaMapLite.get_instance('/mnt/c/Users/eunic/git-repos/cs598dl/public_mm_lite/')
+    os.chdir(ARGS.metamap_lite_dir)
+    mm = MetaMapLite.get_instance(ARGS.metamap_lite_dir)
 
     # observation is a list of spaced separated UMLS CUIs
     df["observations"] = df['TEXT'].apply(lambda x: extract_cui(x))
